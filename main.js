08aec12c6f2333ce431fc014b7145ba5f7a28f8a
ROY <147661241+Knightofthunder@users.noreply.github.com>
2025-11-14 02:42:27 +0530

    initial upload
    update will come soon

---
+++ main.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
+
+
+const appSetting = {
+    databaseURL: "https://todolist-mava-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+const app = initializeApp(appSetting);
+
+const database =  getDatabase(app);
+const cartData = ref(database, "cartDataInfo");
+
+
+const inputButton = document.getElementById("hero-main-addCart-input-button");
+const addToCartButton = document.getElementById("hero-main-addCart-submit-button");
+addToCartButton.addEventListener("click", function hero() {
+ let a = inputButton.value;
+ push(cartData, a);
+ console.log(a);
+});  //working and connected to the index.html and main.css
