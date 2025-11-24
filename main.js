a9fa6050e535be12dc3a0325bffc9091fc16fec5
roy <23mobpsycho@gmail.com>
2025-11-25 01:05:57 +0530

    fuc

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
 
 
 const appSetting = {
@@ -11,14 +11,35 @@ const appSetting = {
 const app = initializeApp(appSetting);
 
 const database =  getDatabase(app);
-const cartData = ref(database, "cartDataInfo");
+//const cartData = ref(database, "cartDataInfo");
+
+
+
+
+
+ const b = document.getElementById("login-input");
+const c = b.value;
+ console.log(c);
+  const submitButton = document.getElementById("login-div-submit");
+  if (submitButton) {
+    submitButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log('Submit button clicked');
+      location.assign('/todo.html');
+    });
+  } else {
+    console.error("Element with id 'login-div-submit' not found");
+  }
+
 
 
 const inputButton = document.getElementById("hero-main-addCart-input-button");
 const addToCartButton = document.getElementById("hero-main-addCart-submit-button");
 addToCartButton.addEventListener("click", function hero() {
  let a = inputButton.value;
- push(cartData, a);
+ //push(cartData, a);
+ set(ref(database, `cartDataInfo/${c}`), a);
+
  console.log(a);
 }
 );
